0cb523dd763891a35d18c746bddf96eb961365f1
lev <Dmslmrk@icloud.com>
2022-10-09 19:42:33 +0300

    Done

---
+++ task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значене N:  ");
+int N= Convert.ToInt32(Console.ReadLine());
+
+
+int NatNumber(int N)
+{
+    int sum=N;
+    if (N!=0)
+    {
+        sum+=NatNumber(N-1);
+    }
+    return sum;
+}
+Console.Write($"Сумма натуральных чисел до числа ({N}) = ");
+Console.Write(NatNumber(N));
---
+++ task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите значене N:  ");
+int N= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значене M:  ");
+int M= Convert.ToInt32(Console.ReadLine());
+int sum=0;
+void NatSum(int N)
+{
+    while (N<=M)
+    if(N>0)
+    {
+        sum+=N;
+        N+=1;
+    }
+    else
+    {
+        N++;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write($"Сумма натуральных чисел от ({N}) до ({M}):    ");
+NatSum(N);
